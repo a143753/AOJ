84619c91fa17290d4c8847d680e9e5346f4c52ae
a143753 <a143753@outlook.jp>
2017-03-01 23:26:22 +0900

    foldl' version still memory limit exceeded.

--- ALDS1_1_D.hs
+++ ALDS1_1_D.hs
@@ -1,45 +1,33 @@
 import Data.List
+import System.IO
 
-max' Nothing  Nothing  = Nothing
-max' (Just a) Nothing  = Just a
-max' Nothing  (Just b) = Just b
-max' (Just a) (Just b) = Just $ max a b
-
-  -- max: 現時点の最大値, s:小さい順に並んだlist, r:大きい順に並んだlist
-search :: Int -> [(Int,Int)] -> [(Int,Int)] -> Maybe Int
-search _ _ [] = Nothing
-search _ [] _ = Nothing
-search cur_max s r =
-  let (ln,lv) = head s
-      (hn,hv) = head r
-      val     = hv - lv
+f (m,a) x =
+  let m' = min m x
+      a' = max a (x-m)
   in
-    if val < cur_max
-    then Nothing -- finish
-    else
-      let new_max = if ln < hn
-                    then max cur_max val
-                    else cur_max
-          s1 = tail s
-          r1 = tail r
-          x0 = search new_max s r1
-          x1 = search new_max s1 r
-      in 
-        if ln < hn
-        then max' (Just val) $ max' x0 x1
-        else max' x0 x1
+    (m',a')
+
+ans :: [Int] -> (Int,Int)
 ans x = 
-    let (x0:x1:_) = x
-        init_max  = x1 - x0
-        s = sortBy (\(na,va) (nb,vb) -> compare va vb) $ zip [1..] x
-        r = reverse s
-        v = max' (Just init_max) (search init_max s r)
-    in
-      v
+  let (x0:y@(x1:xs)) = x
+      init_max  = x1 - x0
+  in
+    foldl' f (x0,init_max) y
+
+exec :: (Int,Int) -> IO (Int,Int)
+exec (m,a)= do
+  l <- getLine
+  let i = read l
+      o = f (m,a) i
+  eof <- isEOF
+  if eof
+  then return o
+  else exec o
 
 main = do
-    n <- getLine
-    c <- getContents
-    let i = map read $ lines c :: [Int]
-        (Just o) = ans i
-    print o
+  n <- getLine
+  c <- getContents
+  let i = map read $ lines c :: [Int]
+      (_,o) = ans i
+  print o
+
