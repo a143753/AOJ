f908d03b2efe862426513f6fdf8f68f7489344f8
a143753 <a143753@outlook.jp>
2017-03-02 22:50:22 +0900

    ALDS1_1_D clear

--- ALDS1_1_D.hs
+++ ALDS1_1_D.hs
@@ -1,24 +1,18 @@
 import Data.List
 import System.IO
 
+f :: (Int,Int) -> Int -> (Int,Int)
 f (m,a) x =
   let m' = min m x
       a' = max a (x-m)
   in
     (m',a')
 
-ans :: [Int] -> (Int,Int)
-ans x = 
-  let (x0:y@(x1:xs)) = x
-      init_max  = x1 - x0
-  in
-    foldl' f (x0,init_max) y
-
 exec :: (Int,Int) -> IO (Int,Int)
 exec (m,a)= do
   l <- getLine
-  let i = read l
-      o = f (m,a) i
+  let i = read l :: Int
+      o = (m,a) `seq` i `seq` f (m,a) i
   eof <- isEOF
   if eof
   then return o
@@ -26,8 +20,5 @@ exec (m,a)= do
 
 main = do
   n <- getLine
-  c <- getContents
-  let i = map read $ lines c :: [Int]
-      (_,o) = ans i
+  (_,o) <- exec (2000000000,-2000000000)
   print o
-
