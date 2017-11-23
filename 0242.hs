59474485bd7b648a5e200ff0657a0479ce4ab4d9
a143753 <a143753@outlook.jp>
2017-11-23 18:36:19 +0900

    solved 0242

--- 0242.hs
+++ 0242.hs
@@ -1,12 +1,24 @@
 import Data.List
 
+pack [] = []
+pack str@(s:_) =
+  let n = length $ filter (== s) str
+      r = filter (/= s) str
+  in
+    (s,n):(pack r)
+
 ans ("0":_) = []
 ans (n:x) =
   let n' = read n :: Int
-      s  = nub $ concat $ map words $ take n' x
+      s  = pack $ concat $ map words $ take n' x
+      s' = sortBy (\ (w0,n0) (w1,n1) -> let c = compare n1 n0
+                                        in if c /= EQ
+                                           then c
+                                           else compare w0 w1 ) s
+                                              
       c  = head $ head $ drop n' x
       r  = drop (n'+1) x
-      a  = take 5 $ sort $ filter (\x -> c == (head x)) s
+      a  = map fst $ take 5 $ filter (\(w,n) -> c == (head w)) s'
       t  = ans r
   in
     if a == []
