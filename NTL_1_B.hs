18e2cfc691d24f6f3d77c62c685657219d8d4724
a143753 <a143753@outlook.jp>
2018-05-03 21:44:57 +0900

    solved NTL_1_B

--- NTL_1_B.hs
+++ NTL_1_B.hs
@@ -1,5 +1,17 @@
+k = 1000000007
+
+ans m 0 = 1
+ans m 1 = m
+
+ans m n =
+  let n' = n `div` 2
+      x  = ans m n'
+      y  = ans m (n-2*n')
+  in
+    (x * x * y) `mod` k
+
 main = do
   l <- getLine
   let (m:n:_) = map read $ words l :: [Integer]
-      o = (m ^ n) `mod` 1000000007
+      o = ans m n
   print o
