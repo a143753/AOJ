2364ec14b207bc52f42bd386b7380fc75bf0bef2
a143753 <a143753@outlook.jp>
2018-10-07 10:53:28 +0900

    still time limit exceeded

--- 2707.hs
+++ 2707.hs
@@ -1,11 +1,11 @@
-skip k l =
-  let a = tail $ take k l
-      r = drop k l
-  in
-    a ++ (skip k r)
+skip k i (l:ls) =
+  if i == 0 then r else l:r
+  where
+    i' = (i + 1) `mod` k
+    r  = skip k i' ls
 
 ans 1 k l = head l
-ans n k l = ans (n-1) k (skip k l)
+ans n k l = ans (n-1) k (skip k 0 l)
 
 main = do
   l <- getLine
