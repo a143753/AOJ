0256d072e9125e128c4cb434570b96811e731000
a143753 <a143753@outlook.jp>
2018-11-23 10:59:49 +0900

    solved 2417

---
+++ 2417.hs
@@ -0,0 +1,23 @@
+import Data.Char
+
+s n = [ "w", "", "k", "s", "t", "n", "h", "m", "y", "r" ]!!n
+
+b 'T' = "a"
+b 'L' = "i"
+b 'U' = "u"  
+b 'R' = "e"  
+b 'D' = "o"
+
+ans1 [] = []
+ans1 ('0':'U':ls) = "n":"n":(ans1 ls)
+ans1 (l:ls) =
+  let n = digitToInt l :: Int
+  in
+    (s n):(ans2 ls)
+
+ans2 (l:ls) = (b l):(ans1 ls)
+
+main = do
+  l <- getLine
+  let o = concat $ ans1 l
+  putStrLn o
