38b87e6eec9e57ad38a8b28b02b9ba1e5b87096a
a143753 <a143753@outlook.jp>
2018-12-14 00:10:03 +0900

    still failed ans 8

--- 1562.hs
+++ 1562.hs
@@ -9,8 +9,43 @@
 とすればよいと考える
 
 -}
+import Data.List
+primes = [2,3,5,7,11]
 
-ans o = 2^(o-1)
+decomp :: [Int] -> Int -> [Int]
+decomp _ 1 = []
+decomp p@(ph:pt) n = 
+    if (n `mod` ph) == 0
+    then ph:(decomp p (n `div` ph) )
+    else decomp pt n
+f :: [Int] -> [Int] -> Int
+f _ [] = 1
+f (p:ps) (v:ds) = p^(v-1) * (f ps ds)
+
+-- |
+-- >>> ans 1
+-- 1
+-- >>> ans 2
+-- 2
+-- >>> ans 3
+-- 4
+-- >>> ans 4
+-- 6
+-- >>> ans 5
+-- 16
+-- >>> ans 6
+-- 12
+-- >>> ans 7
+-- 64
+-- >>> ans 8
+-- 24
+
+ans 1 = 1
+ans n = 
+    let d = reverse $ decomp primes n
+        a = f primes d
+    in
+        a
 
 main = do
   l <- getLine
