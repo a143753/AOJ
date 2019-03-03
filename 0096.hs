917b3c8fef6dc8168f3e74f303a2061384f7c6c8
a143753 <a143753@outlook.jp>
2019-03-03 17:48:28 +0900

    solved 0096

--- 0096.hs
+++ 0096.hs
@@ -1,10 +1,3 @@
-ans' n t [] = 0
-ans' n t (x:xs) =
-  let
-    a = ans (n-1) (t-x)
-  in
-    a + (ans' n t xs)
-
 ans 2 t =
   if t > 2000
   then 0
@@ -12,19 +5,14 @@ ans 2 t =
        then 2001 - t
        else t + 1
 
-ans n t =
- if t > n * 1000
- then 0
- else
-   let m = min t 1000
-   in
---     sum $ map (\x -> ans (n-1) (t-x) ) $ reverse [0..m]
-     ans' n t [0..m]
---     ans' n t $ reverse [0..m]
---     ans' n t $ reverse [0..1000]
+ans 4 t =
+    sum $ map (\i -> let a = ans 2 i
+                         b = ans 2 (t-i)
+                     in
+                         a * b ) [0..t]
 
 main = do
   c <- getContents
-  let i = map read $ lines c :: [Int]
+  let i = map read $ lines c :: [Integer]
       o = map (ans 4) i
   mapM_ print o
