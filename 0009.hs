beef5f9d3b54a756f6c3d7fda13dcaf8648be29f
a143753 <a143753@outlook.jp>
2019-11-03 19:19:09 +0900

    solved 0009

--- 0009.hs
+++ 0009.hs
@@ -1,18 +1,7 @@
---primes :: [Int]
---primes = sieve (2:[3,5..])
---  where
---    sieve (p:xs) = p : sieve [x|x <- xs, x `mod` p > 0]
-
-filout k0 k (x:xs) =
-  if k == x 
-  then filout k0 (k+k0) xs
-  else
-    if k > x
-    then x:(filout k0 k xs)
-    else x:(filout k0 (k+k0) xs)
-
-primes' (x:xs) = x:(primes' $ filout x x xs)
-primes = 2:(primes' [3,5..])
+primes = 2 : 3 : 5 : primes'
+  where
+    isPrime (p:ps) n = p*p > n || n `rem` p /= 0 && isPrime ps n
+    primes' = 7 : filter (isPrime primes') (scanl (+) 11 $ cycle [2,4,2,4,6,2,6,4])
 
 main = do
   l <- getContents
