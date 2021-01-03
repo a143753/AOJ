f5fb58c7b8268d5b556e301018ad9c48dc21a343
a143753 <a143753@outlook.jp>
2021-01-03 09:37:03 +0900

    update prime number generator

--- prime.hs
+++ prime.hs
@@ -1,7 +1,9 @@
-primes = 2 : 3 : 5 : primes'
-  where
-    isPrime (p:ps) n = p*p > n || n `rem` p /= 0 && isPrime ps n
-    primes' = 7 : filter (isPrime primes') (scanl (+) 11 $ cycle [2,4,2,4,6,2,6,4])
+primes :: Integral a => [a]
+primes = map fromIntegral $ [2, 3] ++ primes'
+    where
+        primes' = [5] ++ f 1 7 primes'
+        f m s (p:ps) = [n | n <- ns, gcd m n == 1] ++ f (m * p) (p * p) ps
+            where ns = [x + y | x <- [s, s + 6 .. p * p - 2], y <- [0, 4]]
 
 -- prime factorization
 pfact 1 _ = []
