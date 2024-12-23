73a60e01ad9959d80e0fd6e8b14605d8bc9d6a05
Kazuhisa FUNAMOTO <kazuhisa.funamoto@gmail.com>
2024-12-23 22:24:43 +0900

    solved

---
+++ rust/INFO1_13_C.rs
@@ -0,0 +1,40 @@
+use std::io::BufRead;
+
+fn read_scalar<T: std::str::FromStr>(reader: &mut std::io::StdinLock) -> T
+where
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    let mut scalar = None;
+    reader.lines().take(1).for_each(|line| {
+        scalar = Some(line.unwrap().parse::<T>().unwrap());
+    });
+    scalar.unwrap()
+}
+
+fn read_row_vec<T: std::str::FromStr>(reader: &mut std::io::StdinLock) -> Vec<T>
+where
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    let mut row = Vec::new();
+    reader.lines().take(1).for_each(|line| {
+        row = line
+            .unwrap()
+            .split_whitespace()
+            .map(|x| x.parse::<T>().unwrap())
+            .collect::<Vec<_>>();
+    });
+    row
+}
+
+fn f(v: Vec<i32>) -> i32 {
+    v[0]
+}
+
+fn main() {
+    let reader = std::io::stdin();
+
+    let _ = read_scalar::<i32>(&mut reader.lock());
+    let v = read_row_vec::<i32>(&mut reader.lock());
+
+    println!("{}", f(v));
+}
