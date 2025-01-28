e2420f74abfef410ce1c9f9be00480fd45dae55f
Kazuhisa FUNAMOTO <kazuhisa.funamoto@gmail.com>
2025-01-28 19:35:32 +0900

    solved

---
+++ rust/0094.rs
@@ -0,0 +1,26 @@
+use std::io::BufRead;
+
+fn read_row_vec<T: std::str::FromStr>(reader: &mut std::io::StdinLock) -> Vec<T>
+where
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    let mut row = Vec::new();
+    reader.lines().take(1).for_each(|line| {
+        row = line
+            .unwrap()
+            .split_whitespace()
+            .map(|x| x.parse::<T>().unwrap())
+            .collect::<Vec<_>>();
+    });
+    row
+}
+
+fn main() {
+    let reader = std::io::stdin();
+    let i = read_row_vec::<f32>(&mut reader.lock());
+    let a = i[0];
+    let b = i[1];
+
+    let s = a * b / 3.305785;
+    println!("{}", s);
+}
