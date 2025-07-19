a4840b99017a6cddfee4f762be1c0d9596b6086b
Kazuhisa FUNAMOTO <kazuhisa.funamoto@gmail.com>
2025-07-19 23:46:10 +0900

    solved

--- haskell/INFO1_09_C.hs
+++ haskell/INFO1_09_C.hs
@@ -1,7 +1,7 @@
-import qualified Data.Vector.Mutable as M
-import qualified Data.Vector as V
 import Control.Monad (forM_, when)
-import Control.Monad.ST (runST)
+import Control.Monad.ST (runST,ST)
+import Data.Array.ST (STArray, newArray, readArray, writeArray, freeze)
+import Data.Array (Array, elems)
 
 main :: IO ()
 main = do
@@ -10,11 +10,11 @@ main = do
   contents <- getContents
   let numbers = map read (lines contents) :: [Int]
   let histogram = runST $ do
-        vec <- M.replicate n (0::Int)
+        arr <- newArray (0, n-1) 0 :: ST s (STArray s Int Int)
         forM_ numbers $ \num -> do
-          M.modify vec (+1) num  -- numの値をインクリメント
-        V.freeze vec
-  mapM_ print (V.toList histogram)
-
--- localではcompileに通って期待通りに動作したが,submitするとcompile errorになる。
--- Data.Vector.Mutableがimportできない。
+          when (num >= 0 && num < n) $ do
+            count <- readArray arr num
+            writeArray arr num (count + 1)
+        freeze arr
+  mapM_ print (elems histogram)
+  
