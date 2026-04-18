117c818eed606989643a84b514b2d7d6abaec0d2
a143753 <kazuhisa.funamoto@gmail.com>
2026-04-18 09:35:15 +0900

    solved

--- haskell/0746.hs
+++ haskell/0746.hs
@@ -1,17 +1,23 @@
 import Data.List (sortBy)
 
+rnk :: Int -> [(Int,Int)] -> [(Int,Int,Int)]
+rnk _ [] = []
+rnk r x@((t,n):_) =
+  let y = takeWhile (\(t',n') -> t' == t) x
+      l = length y
+      z = dropWhile (\(t',n') -> t' == t) x
+  in (map (\(t',n') -> (t',n',r)) y) ++ rnk (r+l) z
+
 ans a =
   let t1 = zip a [1..]
       t2 = sortBy (\(x1,y1) (x2,y2) -> compare x1 x2) t1
-      t3 = map (\(x,y) -> (snd x, y, fst x) ) $ zip t2 [1..]
-      t4 = sortBy (\(x1,y1,z1) (x2,y2,z2) -> compare x1 x2) t3
+      t3 = rnk 1 t2
+      t4 = sortBy (\(t1,n1,r1) (t2,n2,r2) -> compare n1 n2) t3
   in
-    t4
-
-    -- 同タイムの人を同じ順位にする処理が必要。
+    map (\(t,n,r) -> r) t4
 
 main = do
   _ <- getLine
   a <- map read <$> words <$> getLine :: IO [Int]
   let o = ans a
-  print o
+  mapM_ print o
